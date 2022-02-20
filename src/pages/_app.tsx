d879f6f749ba1e94c76a4e3b1a70f5e94ee73540
kshida <34312716+kshida@users.noreply.github.com>
2022-02-20 19:28:42 +0900

    add initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import '../styles/index.css'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+const theme = extendTheme({ colors })
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
