d53e33dc549d039a879caf7ce06201ebb654e06a
kshida <34312716+kshida@users.noreply.github.com>
2022-03-27 23:01:17 +0900

    next-seoを導入

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+export default {
+  titleTemplate: '%s | プログラミングを知るはプログラミングを行うに如かず',
+  defaultTitle: 'プログラミングを知るはプログラミングを行うに如かず',
+  description: '文系エンジニアが日々の学びを分かりやすい形でアウトプットしていきます。',
+  openGraph: {
+    site_name: 'プログラミングを知るはプログラミングを行うに如かず',
+    title: 'プログラミングを知るはプログラミングを行うに如かず',
+    type: 'website',
+    description: '文系エンジニアが日々の学びを分かりやすい形でアウトプットしていきます。',
+    url: 'https://programming-jissen.com/',
+    locale: 'ja_JP',
+    images: [
+      {
+        url: 'https://avatars.githubusercontent.com/u/34312716?v=4',
+        width: 1200,
+        height: 630,
+        alt: '',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@kshida39',
+    site: '@kshida39',
+    cardType: 'summary_large_image',
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import 'styles/index.css'
+import SEO from '../../next-seo.config'
 
 const colors = {
   brand: {
@@ -14,6 +16,7 @@ const theme = extendTheme({ colors })
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
