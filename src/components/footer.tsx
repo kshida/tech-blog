3488e4bc4925796453bca0056bfc866fba8c670a
kshida <34312716+kshida@users.noreply.github.com>
2022-04-10 00:53:14 +0900

    ヘッダーとフッターを調整

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,97 +1,33 @@
 import {
   Box,
-  chakra,
   Container,
   Stack,
   Text,
   useColorModeValue,
-  VisuallyHidden,
 } from '@chakra-ui/react'
-import { FaGithub, FaTwitter, FaYoutube } from 'react-icons/fa'
-import { SiZenn } from 'react-icons/si'
 import NavLink from 'components/nav-link'
-
-type Props = {
-  label: string,
-  href: string,
-  children: React.ReactNode
-}
-
-const SocialButton = ({
-  children,
-  label,
-  href,
-}: Props) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
-      w={8}
-      h={8}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      target={'_blank'}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
-      _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}>
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  )
-}
 
 const Footer = () => {
   return (
     <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
+      px={6}
+      bg={useColorModeValue('#9dd3a8', 'gray.900')}
       color={useColorModeValue('gray.700', 'gray.200')}>
       <Container
         as={Stack}
-        maxW={'6xl'}
-        py={4}
+        maxW={'initial'}
+        px={0}
+        py={3}
+        direction={{ base: 'column', md: 'row' }}
         spacing={4}
-        justify={'center'}
+        justify={{ base: 'center', md: 'space-between' }}
         align={'center'}>
-        <Stack direction={'row'} spacing={6}>
-          <SocialButton label={'GitHub'} href={'https://github.com/kshida'}>
-            <FaGithub />
-          </SocialButton>
-          <SocialButton label={'Twitter'} href={'https://twitter.com/kshida39'}>
-            <FaTwitter />
-          </SocialButton>
-          <SocialButton label={'Zenn'} href={'https://zenn.dev/kshida'}>
-            <SiZenn />
-          </SocialButton>
-          <SocialButton label={'YouTube'} href={'#'}>
-            <FaYoutube />
-          </SocialButton>
+        <Stack direction={'row'} spacing={1} className='text-xs'>
+          <NavLink link='/privacy-policy' isRichStyle={true}>プライバシーポリシー</NavLink>
+          <NavLink link='/disclaimer' isRichStyle={true}>免責事項</NavLink>
         </Stack>
+        <Text fontSize='xs'>©2022 kshida's blog</Text>
       </Container>
-
-      <Box
-        borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
-        <Container
-          as={Stack}
-          maxW={'6xl'}
-          py={3}
-          direction={{ base: 'column', md: 'row' }}
-          spacing={4}
-          justify={{ base: 'center', md: 'space-between' }}
-          align={{ base: 'center', md: 'center' }}>
-          <Stack direction={'row'} spacing={1} className='text-xs'>
-            <NavLink link='/privacy-policy' isRichStyle={true}>プライバシーポリシー</NavLink>
-            <NavLink link='/disclaimer' isRichStyle={true}>免責事項</NavLink>
-          </Stack>
-          <Text fontSize='xs'>©2017-2022 プログラミングを知るはプログラミングを行うに如かず</Text>
-        </Container>
-      </Box>
     </Box>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,39 +1,125 @@
 import {
   Box,
-  Container,
   Heading,
   useColorModeValue,
   Stack,
+  Flex,
+  HStack,
+  IconButton,
+  useDisclosure,
+  chakra,
+  VisuallyHidden,
 } from '@chakra-ui/react'
 import { Links } from 'lib/links'
 import NavLink from 'components/nav-link'
+import { FaGithub, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { SiZenn } from 'react-icons/si'
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
+
+type Props = {
+  label: string,
+  href: string,
+  children: React.ReactNode
+}
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: Props) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      target={'_blank'}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  )
+}
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Box
-        bg={useColorModeValue('gray.100', 'gray.900')}
-        px={4}>
-        <Heading as="h1" size='lg' py={4} textAlign={'center'} wordBreak='keep-all'>
-          プログラミングを知るは プログラミングを行うに如かず
-        </Heading>
-      </Box>
-      <Box
-        bg={useColorModeValue('gray.50', 'gray.900')}
-        color={useColorModeValue('gray.700', 'gray.200')}>
-        <Container
-          as={Stack}
-          maxW={'6xl'}
-          py={2}
-          spacing={4}
-          justify={'center'}
-          align={'center'}>
-          <Stack direction={'row'} spacing={6}>
-            {Links.map((link) => (
-              <NavLink key={link.title} link={link.slug} isRichStyle={true}>{link.title}</NavLink>
-            ))}
-          </Stack>
-        </Container>
+        bg={useColorModeValue('#9dd3a8', 'gray.900')}
+        px={6}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <HStack spacing={8} alignItems={'center'}>
+            <Heading as="h1" size='lg' py={4} textAlign={'center'} wordBreak='keep-all'>
+              kshida's blog
+            </Heading>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link.title} link={link.slug} isRichStyle={true}>{link.title}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <HStack spacing={4} display={{ base: 'none', md: 'flex' }}>
+            <Stack direction={'row'} spacing={6}>
+              <SocialButton label={'GitHub'} href={'https://github.com/kshida'}>
+                <FaGithub />
+              </SocialButton>
+              <SocialButton label={'Twitter'} href={'https://twitter.com/kshida39'}>
+                <FaTwitter />
+              </SocialButton>
+              <SocialButton label={'Zenn'} href={'https://zenn.dev/kshida'}>
+                <SiZenn />
+              </SocialButton>
+              <SocialButton label={'YouTube'} href={'#'}>
+                <FaYoutube />
+              </SocialButton>
+            </Stack>
+          </HStack>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link.title} link={link.slug} isRichStyle={true}>{link.title}</NavLink>
+              ))}
+              <Stack direction={'row'} spacing={6}>
+                <SocialButton label={'GitHub'} href={'https://github.com/kshida'}>
+                  <FaGithub />
+                </SocialButton>
+                <SocialButton label={'Twitter'} href={'https://twitter.com/kshida39'}>
+                  <FaTwitter />
+                </SocialButton>
+                <SocialButton label={'Zenn'} href={'https://zenn.dev/kshida'}>
+                  <SiZenn />
+                </SocialButton>
+                <SocialButton label={'YouTube'} href={'#'}>
+                  <FaYoutube />
+                </SocialButton>
+              </Stack>
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
     </>
   )
