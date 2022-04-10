bce0764c6b1b93862eb3409ad3e3207285be0a4e
kshida <34312716+kshida@users.noreply.github.com>
2022-04-11 00:43:41 +0900

    目次生成機能を追加

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,17 @@
 import { remark } from 'remark'
 import html from 'remark-html'
+import slug from 'remark-slug'
+import toc from 'remark-toc'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark()
+    .use(slug)
+    .use(toc, {
+      heading: '目次',
+      prefix: 'user-content-',
+      maxDepth: 2
+    })
+    .use(html)
+    .process(markdown)
   return result.toString()
 }
