8c47b1241a82d5e78de660804bc27e8dad7b745a
kshida <34312716+kshida@users.noreply.github.com>
2022-10-26 11:51:25 +0900

    不要なファイルを削除

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { Meta } from '@/components/Meta'
 
 interface Props {
   preview?: boolean
@@ -10,7 +9,6 @@ interface Props {
 export const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <Meta />
       <Header />
       <div className='min-h-[calc(100vh-64px-48px)]'>{children}</div>
       <Footer />
